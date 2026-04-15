baade153d315a6274fd075a4bb04c145a8e0c279
Tim VanDoren <master-elodin@users.noreply.github.com>
2026-04-15 09:52:25 -0700

    add item-get

--- src/tampermonkey-raw.js
+++ src/tampermonkey-raw.js
@@ -8,5 +8,20 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    console.log('hello world!')
+    let retries = 0;
+    const interval = setInterval(() => {
+        const items = jQuery('.some-item');
+        if (retries++ > 20) {
+            console.log('give up');
+            clearInterval(interval);
+            return;
+        }
+        if (!items.length) {
+            return;
+        }
+        items.each((i, el) => {
+            const asEl = jQuery(el);
+            // do something with asEl
+        });
+    }, 500);
 })();
