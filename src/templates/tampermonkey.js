0c4791a598cbc1c8f7f8a43cd1366afca9aaf735
Tim VanDoren <master-elodin@users.noreply.github.com>
2026-04-15 09:20:13 -0700

    Remove update URL from tampermonkey.js template

--- src/templates/tampermonkey.js
+++ src/templates/tampermonkey.js
@@ -1,7 +1,6 @@
 // ==UserScript==
 // @name         ${name}
 // @version      ${version}
-// @updateURL    https://confluence-tools.swacorp.com/pages/viewpage.action?pageId=340558277
 // @description  ${description}
 // @author       ${author}
 // @include      ${target}
