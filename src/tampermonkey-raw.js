a486465267dd28b0d62b23764333ab710ccf0bce
Tim VanDoren <master-elodin@users.noreply.github.com>
2026-04-15 09:40:20 -0700

    add raw tampermonkey template

---
+++ src/tampermonkey-raw.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         ${name}
+// @version      ${version}
+// @description  ${description}
+// @author       ${author}
+// @include      ${target}
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.slim.min.js
+// ==/UserScript==
+(function() {
+    'use strict';
+    console.log('hello world!')
+})();
